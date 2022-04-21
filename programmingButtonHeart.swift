38ff1c989e58a8dbd35712028bf2eaa291a89a9d
alank101 <99371673+alank101@users.noreply.github.com>
2022-04-21 17:26:04 -0500

    Add files via upload

---
+++ programmingButtonHeart.swift
@@ -0,0 +1,51 @@
+//
+//  appButtons.swift
+//  Stacks Coding Challenge
+//
+//  Created by admin on 4/10/22.
+//
+
+import SwiftUI
+
+struct programmingButtonHeart: View {
+    var images:Image = Image(systemName: "laptopcomputer")
+    var mainText:String = "Programming"
+    var subText:String = "image, create, share, enjoy"
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(.blue)
+            VStack {
+                HStack(alignment: .top) {
+                    Spacer()
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                        .padding()
+                }
+                Spacer()
+                VStack {
+                    images
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:50)
+                        .foregroundColor(.black)
+                    Text(mainText)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                    Text(subText)
+                        .font(.caption)
+                        .foregroundColor(.black)
+                        .padding()
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct programmingButtonHeart_Previews: PreviewProvider {
+    static var previews: some View {
+        appButtons()
+    }
+}
